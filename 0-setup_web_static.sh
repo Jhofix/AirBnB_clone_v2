bd4f610664dcb3bd1373ac579e0b16376b7b52f5
Jhofix <dadajoshua518@gmail.com>
2024-09-11 23:40:12 +0100

    "task0"

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,52 +2,35 @@
 # Prepare a web server for deployment
 
 if ! [ -f /usr/sbin/nginx ]; then
-    apt-get -y update
+    apt-get -y update && apt-get -y update
     apt-get -y install nginx
     ufw allow 'Nginx HTTP'
     echo "Hello World!" > /var/www/html/index.nginx-debian.html
-    service nginx start
 fi
 
-string="\tif (\$request_filename ~ redirect_me){\
-		\n\t\trewrite ^ https://sketchfab.com/bluepeno/models permanent;\
-	\n\t}\n\
-    \n\tadd_header X-Served-By $HOSTNAME;\
-	\n\terror_page 404 /error404.html;\
-	\n\tlocation = /error404.html {\
-		\n\t\troot /var/www/html;\
-		\n\t\tinternal;\
-	\n\t}"
+string="\t}\n\
+    \n\
+    location /hbnb_static {\n\
+        alias data/web_static/current/;\n\
+    }\n\
+    \n\
+    if (\$request_filename ~ redirect_me){\n\
+        rewrite ^ https://sketchfab.com/bluepeno/models permanent;\n\
+    }\n\
+    \n\
+    add_header X-Served-By $HOSTNAME;\n\
+    error_page 404 /error404.html;\n\
+    location = /error404.html {\n\
+        root /var/www/html;\n\
+        internal;\n\
+    }"
+
+sed -i "1,/^\t}/ s|^\t}|$string|" /etc/nginx/sites-enabled/default
 
 echo "Ceci n'est pas une page" > /var/www/html/error404.html
-sed -i "53 i\ $string" /etc/nginx/sites-enabled/default
 
-if [ "$(pgrep -c nginx)" -lt 1 ]; then
-	service nginx start
-else
-	service nginx restart
-fi
-
-if ! [ -e /data/ ]; then
-    mkdir /data/
-
-    if ! [ -e /data/web_static/ ]; then
-        mkdir /data/web_static/
-    fi
-
-    if ! [ -e /data/web_static/releases/ ]; then
-        mkdir /data/web_static/releases/
-    fi
-
-    if ! [ -e /data/web_static/shared/ ]; then
-        mkdir /data/web_static/shared/
-    fi
-
-    if ! [ -e /data/web_static/releases/test/ ]; then
-        mkdir /data/web_static/releases/test/
-    fi
-fi
-
+mkdir /data/web_static/releases/test/
+mkdir /data/web_static/shared/
 echo "Ready for web_static deployment" > /data/web_static/releases/test/index.html
 
 if [ -e /data/web_static/current ]; then
@@ -57,11 +40,9 @@ else
 fi
 
 chown ubuntu:ubuntu /data/
-alias="\t}\n\
-    location \/hbnb_static {\n\
-        alias data\/web_static\/current\/;\n\
-    }\n"
 
-sed "1,/^\t}/ s/^\t}/$alias/" /etc/nginx/sites-enabled/efault
-
-sudo service nginx restart
+if [ "$(pgrep -c nginx)" -lt 1 ]; then
+	service nginx start
+else
+	service nginx restart
+fi
