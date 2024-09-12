cea67e1d9bf0752b772b17545d28c9eda5a91e66
Jhofix <dadajoshua518@gmail.com>
2024-09-12 17:10:55 +0100

    "task1"

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,13 +9,13 @@ import os.path
 def do_pack():
     """Archive all files in web_static"""
     time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    path=f"versions/web_static_{time_stamp}.tar"
+    path=f"versions/web_static_{time_stamp}.tgz"
     print(path)
     ok = "Good"
 
     if not os.path.isdir("versions"):
         if local("mkdir versions").failed:
             return None
-    if local(f"tar -cvf {path}").failed:
+    if local(f"tar -czvf {path} web_static").failed:
         return None
     return os.path.abspath(path)
