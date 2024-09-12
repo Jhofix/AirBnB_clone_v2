4ee85e55cf8011d26d1835f4f14f6ce6a51bd965
Jhofix <dadajoshua518@gmail.com>
2024-09-12 08:13:08 +0100

    "task1"

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,16 +4,18 @@ from datetime import datetime
 from fabric.api import *
 import os.path
 
+
 def do_pack():
     """Archive all files in web_static"""
     time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    
+
     if not os.path.isdir("versions"):
         if local("mkdir versions").failed:
             return None
-        else: 
+        else:
             print("SUCCEESSFUL: Created directory(s) successfully")
-    
+
     if os.path.isdir("versions"):
-        if not local("tar -cvf versions/web_static_{}.tar *".format(time_stamp)):
+        if not local("tar -cvf versions/web_static_{}.tar *"
+                     .format(time_stamp)):
             return None
